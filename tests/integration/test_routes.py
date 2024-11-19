69af25980721e8871c091da95f317b85b6e840b9
jeremyarancio <jeremyarancio@gmail.com>
2024-11-19 18:44:23 +0100

    fix:

--- tests/integration/test_routes.py
+++ tests/integration/test_routes.py
@@ -1,6 +1,4 @@
 from unittest.mock import Mock
-from io import BytesIO
-from typing import BinaryIO
 import datetime
 
 import pytest
@@ -10,7 +8,7 @@ from sqlmodel import Session, SQLModel, create_engine, StaticPool
 from invoice_reader.app.routes import app
 from invoice_reader.db import get_session
 from invoice_reader import models  # noqa: F401
-from invoice_reader.schemas import InvoiceSchema, ClientAdresseSchema, RevenuSchema
+from invoice_reader.schemas import InvoiceSchema, FileData
 
 
 @pytest.fixture(name="session")  
@@ -36,43 +34,67 @@ def client_fixture(session: Session):
 
 
 @pytest.fixture
-def file():
-    return BytesIO(b"Files")
+def files():
+    with open("tests/assets/paper.pdf", "rb") as f:
+        files = {"upload_file": ("filename", f, "application/pdf")}
+        yield files
 
 
 @pytest.fixture
-def s3_mocker(mocker):
+def file_data() -> FileData:
+    return FileData(
+        user_id="jeremy1234",
+        filename="filename.pdf"
+    )
+
+
+@pytest.fixture
+def s3_suffix(file_data: FileData) -> str:
+    return f"{file_data.user_id}/{file_data.file_id}{file_data.file_format}"
+
+
+@pytest.fixture
+def s3_mocker(mocker) -> Mock:
     mock_client = Mock()
     mocker.patch("boto3.client", return_value=mock_client)
     return mock_client
 
 
 @pytest.fixture
+def bucket() -> str:
+    return "bucket"
+
+
+@pytest.fixture
 def invoice_schema():
     return InvoiceSchema(
-        client_adresse=ClientAdresseSchema(
-            street_number="19",
-            street_name="road of coal",
-            city="Carcassone",
-            country="France",
-            zipcode=45777
-        ),
+        street_number="19",
+        street_name="road of coal",
+        city="Carcassone",
+        country="France",
+        zipcode=45777,
         client_name="Sacha&Cie",
         invoiced_date=datetime.date(2024, 11, 18),
         number="14SQ456",
-        revenu=RevenuSchema(
-            excluding_tax=10000,
-            currency="€",
-            vat="20",
-        ),
+        excluding_tax=10000,
+        # currency="€",
+        vat="20",
     )
 
 
 def test_add_invoice(
-    file: BinaryIO,
-    invoice_schema: InvoiceSchema, 
+    files,
     client: TestClient, 
-    s3_mocker: Mock
+    s3_mocker: Mock,
+    invoice_schema: InvoiceSchema,
 ):
-    client.post("api/v1/add", file=file)
-
+    data = invoice_schema.model_dump()
+    data = {k: (str(v) if isinstance(v, datetime.date) else v) for k, v in data.items()}
+    response = client.post(
+        url="api/v1/add/",
+        # headers = {"Content-Type": "multipart/form-data"},
+        data=data,
+        files=files
+    )
+    assert response.status_code == 200
+    s3_mocker.assert_called_once()
