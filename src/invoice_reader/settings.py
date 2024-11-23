9c2370b05c5ad635415989174932dcfe7b8c08a9
jeremyarancio <jeremyarancio@gmail.com>
2024-11-23 18:39:22 +0100

    Because I can

--- src/invoice_reader/settings.py
+++ src/invoice_reader/settings.py
@@ -14,3 +14,8 @@ DATABASE_URL = os.getenv("DATABASE_URL", f"sqlite:///{REPO_DIR / "data/database.
 
 # DEBUG
 _USER_ID = uuid.UUID("cb7828b2-a187-4bdf-b2f8-02edfdc1f159")
+
+# Security
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 15)
