9dca45726174deefa2dccd602c34742aef4341c7
jeremyarancio <jeremyarancio@gmail.com>
2025-04-30 10:37:42 +0200

    fix: :bug: Issue with retrieving exisiting elements from database -> 409 issues
    Comes from SQLModel where (&) badly written

--- ui/src/common/hooks.ts
+++ ui/src/common/hooks.ts
@@ -1,7 +1,9 @@
 import { useAuth } from "@/common/components/AuthProvider";
+import { queryClient } from "@/services/api";
 
 export const useSignOut = () => {
     const { setToken } = useAuth();
+    queryClient.clear();
     return () => {
         setToken(undefined); // undefined = Log out
     };
