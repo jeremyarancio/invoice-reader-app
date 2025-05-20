69be11ed358534b7aad83d7a3c4a5794c5f0dc5a
jeremyarancio <jeremyarancio@gmail.com>
2025-05-20 14:23:13 +0200

    fix: :bug: Fix bug presigned url not working

--- server/src/invoice_reader/infrastructure/storage.py
+++ server/src/invoice_reader/infrastructure/storage.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from typing import BinaryIO
 
 import boto3
-from botocore.exceptions import ClientError
+from botocore.config import Config
 
 from invoice_reader import settings
 
@@ -20,28 +20,19 @@ class S3:
 
     def store(self, file: BinaryIO, suffix: str) -> None:
         s3_client = boto3.client("s3")
-        try:
-            s3_client.upload_fileobj(file, self.bucket, suffix)
-        except ClientError:
-            raise
+        s3_client.upload_fileobj(file, self.bucket, suffix)
 
     def delete(self, suffix: str) -> None:
         s3_client = boto3.client("s3")
-        try:
-            s3_client.delete_object(Bucket=self.bucket, Key=suffix)
-        except ClientError:
-            raise
+        s3_client.delete_object(Bucket=self.bucket, Key=suffix)
 
     def create_presigned_url(
         self, suffix: str, expiration: int = settings.PRESIGNED_URL_EXPIRATION
     ) -> str:
-        s3_client = boto3.client("s3")
-        try:
-            response: str = s3_client.generate_presigned_url(
-                "get_object",
-                Params={"Bucket": self.bucket, "Key": suffix},
-                ExpiresIn=expiration,
-            )
-            return response
-        except ClientError:
-            raise
+        s3_client = boto3.client("s3", config=Config(signature_version="s3v4"))
+        response: str = s3_client.generate_presigned_url(
+            "get_object",
+            Params={"Bucket": self.bucket, "Key": suffix},
+            ExpiresIn=expiration,
+        )
+        return response
