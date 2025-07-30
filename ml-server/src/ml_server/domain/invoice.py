48ce00e0b9dafc5440ac6d0d760788ea70b1287f
jeremyarancio <jeremy_arancio@epam.com>
2025-07-30 19:02:09 +0200

    feat: :memo: Pydantic validation for structured output
    Validate or return default|None

--- ml-server/src/ml_server/domain/invoice.py
+++ ml-server/src/ml_server/domain/invoice.py
@@ -1,13 +1,35 @@
-from datetime import date
-from enum import StrEnum
+from datetime import date, datetime
+from typing import Annotated
 
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, BeforeValidator, ValidationInfo, field_validator
+
+CURRENCIES = ["USD", "EUR", "GBP"]
 
 
-class Currency(StrEnum):
-    USD = "USD"
-    EUR = "EUR"
-    GBP = "GBP"
+def _is_positive_integer(value: int | None) -> int | None:
+    return int(value) if value and value >= 0 else None
+
+
+def _is_positive_float(value: float | None) -> float | None:
+    return float(value) if value and value >= 0 else None
+
+
+def _is_valid_currency(value: str | None) -> str | None:
+    if value and value.upper() in CURRENCIES:
+        return value.upper()
+    return None
+
+
+def _is_valid_date(value: str | date | None) -> date | None:
+    if value:
+        if isinstance(value, date):
+            return value
+        else:
+            try:
+                return datetime.strptime(value, "%d/%m/%Y").date()
+            except (ValueError, TypeError):
+                return None
+    return None
 
 
 class Address(BaseModel):
@@ -18,19 +40,23 @@ class Address(BaseModel):
 
 
 class Invoice(BaseModel):
-    gross_amount: float | None = None
-    vat: int | None = None
-    issued_date: date | None = None
+    gross_amount: Annotated[float | None, BeforeValidator(_is_positive_float)] = None
+    vat: Annotated[int | None, BeforeValidator(_is_positive_integer)] = None
+    issued_date: Annotated[date | None, BeforeValidator(_is_valid_date)] = None
+    due_date: date | None = None
     invoice_number: str | None = None
     invoice_description: str | None = None
-    currency: Currency | None = None
+    currency: Annotated[str | None, BeforeValidator(_is_valid_currency)] = None
 
-    @field_validator("vat", mode="before")
+    @field_validator("due_date", mode="before")
     @classmethod
-    def validate_vat(cls, v):
-        if v is None or v >= 0:
-            return v
-        return None
+    def validate_due_date(cls, value, info: ValidationInfo) -> date | None:
+        _date = _is_valid_date(value)
+        if _date:
+            issued_date = info.data.get("issued_date")
+            if issued_date and _date > issued_date:
+                return _date
+            return None
 
 
 class Client(BaseModel):
